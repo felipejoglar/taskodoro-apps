d6c27f4e495dd6be31091965fb60db0667bf39d4
Felipe Joglar <felipejoglar@gmail.com>
2022-02-09 10:48:46 +0100

    Create android project

---
+++ apps/android/taskodoro/build.gradle.kts
@@ -0,0 +1,84 @@
+/*
+ *    Copyright 2022 Felipe Joglar
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+
+    compileSdk = 31
+    buildToolsVersion = "32.0.0"
+
+    defaultConfig {
+        applicationId = "com.taskodoro.android.app"
+
+        minSdk = 26
+        targetSdk = 31
+
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.0-rc03"
+    }
+
+    packagingOptions {
+        excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.7.0")
+
+    implementation("androidx.compose.ui:ui:1.1.0-rc03")
+    implementation("androidx.compose.material:material:1.1.0-rc03")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.1.0-rc03")
+    implementation("androidx.activity:activity-compose:1.4.0")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+
+    testImplementation("junit:junit:4.13.2")
+
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.0-rc03")
+
+    debugImplementation("androidx.compose.ui:ui-tooling:1.1.0-rc03")
+}
