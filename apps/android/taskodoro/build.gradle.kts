62e824dd86d81f26a2b018c9d60f04008c62c9b8
Felipe Joglar <felipejoglar@gmail.com>
2022-02-10 22:43:00 +0100

    Update compose to 1.1.0

--- apps/android/taskodoro/build.gradle.kts
+++ apps/android/taskodoro/build.gradle.kts
@@ -55,7 +55,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.1.0-rc03"
+        kotlinCompilerExtensionVersion = "1.1.0"
     }
 
     packagingOptions {
@@ -67,9 +67,9 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.7.0")
 
-    implementation("androidx.compose.ui:ui:1.1.0-rc03")
-    implementation("androidx.compose.material:material:1.1.0-rc03")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.1.0-rc03")
+    implementation("androidx.compose.ui:ui:1.1.0")
+    implementation("androidx.compose.material:material:1.1.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.1.0")
     implementation("androidx.activity:activity-compose:1.4.0")
 
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
@@ -78,7 +78,7 @@ dependencies {
 
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.0-rc03")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.0")
 
-    debugImplementation("androidx.compose.ui:ui-tooling:1.1.0-rc03")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.1.0")
 }
