23db94df2036c954a5fb2267ba45f3f7772f9b2e
Felipe Joglar <felipejoglar@gmail.com>
2023-11-13 16:56:46 +0100

    Apply new color scheme

--- apps/android/app/src/main/kotlin/com/taskodoro/android/app/ui/theme/Color.kt
+++ apps/android/app/src/main/kotlin/com/taskodoro/android/app/ui/theme/Color.kt
@@ -19,61 +19,26 @@ package com.taskodoro.android.app.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // Light Theme Colors
-val ForegroundDefaultLight = Color(red = 31, green = 35, blue = 40)
-val BackgroundDefaultLight = Color(red = 255, green = 255, blue = 255)
-val ForegroundMutedLight = Color(red = 73, green = 75, blue = 85)
-val BackgroundSubtleLight = Color(red = 246, green = 248, blue = 250)
-
-val GreenAccentLight = Color(red = 26, green = 127, blue = 55)
-val GreenBackgroundLight = Color(red = 218, green = 251, blue = 225)
-val GreenBackgroundEmphasisLight = Color(red = 31, green = 136, blue = 61)
-val GreenBorderLight = Color(red = 74, green = 194, blue = 107).copy(alpha = 0.4f)
-val GreenBorderEmphasisLight = GreenBackgroundEmphasisLight
-
-val BrownAccentLight = Color(red = 154, green = 103, blue = 0)
-val BrownBackgroundLight = Color(red = 255, green = 248, blue = 197)
-val BrownBackgroundEmphasisLight = Color(red = 154, green = 103, blue = 0)
-val BrownBorderLight = Color(red = 212, green = 167, blue = 44).copy(alpha = 0.4f)
-val BrownBorderEmphasisLight = BrownBackgroundEmphasisLight
-
-val PinkAccentLight = Color(red = 191, green = 57, blue = 137)
-val PinkBackgroundLight = Color(red = 255, green = 239, blue = 247)
-val PinkBackgroundEmphasisLight = Color(red = 191, green = 57, blue = 137)
-val PinkBorderLight = Color(red = 255, green = 128, blue = 200).copy(alpha = 0.4f)
-val PinkBorderEmphasisLight = PinkBackgroundEmphasisLight
-
-val RedAccentLight = Color(red = 209, green = 36, blue = 47)
-val RedBackgroundLight = Color(red = 255, green = 235, blue = 233)
-val RedBackgroundEmphasisLight = Color(red = 207, green = 34, blue = 46)
-val RedBorderLight = Color(red = 255, green = 129, blue = 130).copy(alpha = 0.4f)
-val RedBorderEmphasisLight = RedBackgroundEmphasisLight
+val BackgroundDefaultLight = Color(0xFFEFF0F1)
+val BackgroundSubtleLight = Color(0xFFD0D3D4)
+val ForegroundDefaultLight = Color(0xFF1F2122)
+val ForegroundSubtleLight = Color(0xFF494D4F)
 
 // Dark Theme Colors
-val ForegroundDefaultDark = Color(red = 230, green = 237, blue = 243)
-val BackgroundDefaultDark = Color(red = 13, green = 17, blue = 23)
-val ForegroundMutedDark = Color(red = 159, green = 161, blue = 173)
-val BackgroundSubtleDark = Color(red = 22, green = 27, blue = 34)
+val BackgroundDefaultDark = Color(0xFF1F2122)
+val BackgroundSubtleDark = Color(0xFF494D4F)
+val ForegroundDefaultDark = Color(0xFFEFF0F1)
+val ForegroundSubtleDark = Color(0xFFD0D3D4)
 
-val GreenAccentDark = Color(red = 63, green = 185, blue = 80)
-val GreenBackgroundDark = Color(red = 46, green = 160, blue = 67).copy(alpha = 0.15f)
-val GreenBackgroundEmphasisDark = Color(red = 55, green = 134, blue = 54)
-val GreenBorderDark = Color(red = 46, green = 160, blue = 67).copy(alpha = 0.4f)
-val GreenBorderEmphasisDark = GreenBackgroundEmphasisDark
+// Common Palette
+val DarkCyanAccent = Color(0xFF099487)
+val DarkCyanBackground = Color(0xFFCFFCF8)
 
-val BrownAccentDark = Color(red = 210, green = 153, blue = 34)
-val BrownBackgroundDark = Color(red = 187, green = 128, blue = 9).copy(alpha = 0.15f)
-val BrownBackgroundEmphasisDark = Color(red = 158, green = 106, blue = 3)
-val BrownBorderDark = Color(red = 187, green = 128, blue = 9).copy(alpha = 0.4f)
-val BrownBorderEmphasisDark = BrownBackgroundEmphasisDark
+val TurquoiseAccent = Color(0xFF00D4BF)
+val TurquoiseBackground = Color(0xFFD5FFFB)
 
-val PinkAccentDark = Color(red = 219, green = 97, blue = 162)
-val PinkBackgroundDark = Color(red = 219, green = 97, blue = 162).copy(alpha = 0.1f)
-val PinkBackgroundEmphasisDark = Color(red = 191, green = 75, blue = 138)
-val PinkBorderDark = Color(red = 219, green = 97, blue = 162).copy(alpha = 0.4f)
-val PinkBorderEmphasisDark = PinkBackgroundEmphasisDark
+val AureolinAccent = Color(0xFFFBE81F)
+val AureolinBackground = Color(0xFFFEFCDF)
 
-val RedAccentDark = Color(red = 248, green = 81, blue = 73)
-val RedBackgroundDark = Color(red = 248, green = 81, blue = 73).copy(alpha = 0.1f)
-val RedBackgroundEmphasisDark = Color(red = 218, green = 54, blue = 51)
-val RedBorderDark = Color(red = 248, green = 81, blue = 73).copy(alpha = 0.4f)
-val RedBorderEmphasisDark = RedBackgroundEmphasisDark
+val PoppyAccent = Color(0xFFDC4949)
+val PoppyBackground = Color(0xFFFAE5E5)
